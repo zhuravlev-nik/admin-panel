a377d582afabf8b709d12051123ece0d34da17db
Nikolay Zhuravlev <Nikolay.A.Zhuravlev@gmail.com>
2017-12-24 21:46:50 +0300

    * Create express app * Create routes (root, campaign/new, banner/new) * Create templates (main, campaign, banner) * Add tmp campaign/banners object

---
+++ app.js
@@ -0,0 +1,30 @@
+const
+  express = require('express'),
+  app = express(),
+  path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
---
+++ routes/index.js
@@ -0,0 +1,88 @@
+const
+  express = require('express'),
+  router = express.Router();
+
+// while not connested to mongodb
+let campaignList = [
+  {_id:'111',name: 'Campaign 1', active: true, created: new Date, banners: [{_id: '111', name: 'Banner 1_1', active: true}, {_id: '222', name: 'Banner 1_2', active: false}, {_id: '333', name: 'Banner 1_3', active: true}]},
+  {_id:'222',name: 'Campaign 2', active: false, created: new Date, banners: [{_id: '111', name: 'Banner 2_1', active: true}, {_id: '222', name: 'Banner 2_2', active: true}]},
+  {_id:'333',name: 'Campaign 3', active: true, created: new Date, banners: [{_id: '111', name: 'Banner 3_1', active: true}, {_id: '222', name: 'Banner 3_2', active: true}]},
+  {_id:'444',name: 'Campaign 4', active: true, created: new Date, banners: [{_id: '111', name: 'Banner 4_1', active: true}, {_id: '222', name: 'Banner 4_2', active: true}]},
+  {_id:'555',name: 'Campaign 5', active: true, created: new Date, banners: [{_id: '111', name: 'Banner 5_1', active: true}, {_id: '222', name: 'Banner 5_2', active: true}]}
+];
+ 
+
+router.use(function(req, res, next){
+  res.locals.breadcrumbs = [{name:'Main',path:'/'}];
+  return next();
+});
+
+router.route('/') // root route (campaigns list)
+  .all(function(req, res, next){
+    res.locals.title = 'Campaign list';
+    res.locals.list = campaignList;
+    return next();
+  })
+  .get(function(req, res, next){
+    res.render('index');
+  });
+
+router.route('/api/')
+  .all(function(req, res, next){
+     res.json({})
+   })
+
+router.route('/((:campaignId)|new)/') // View and edit campaign info, view campaign banners
+  .all(function(req, res, next){
+    console.log(req.params.campaignId);
+    res.locals.title = 'Campaign data';
+    res.locals.baseUrl = '/'+req.params.campaignId+'/';
+    for(let i in campaignList){
+      if(campaignList[i]._id == req.params.campaignId){
+        res.locals.campaign = campaignList[i];
+      }
+    }
+    if(!res.locals.campaign) res.locals.campaign = {};
+    res.locals.breadcrumbs.push({name: res.locals.campaign.name || 'New campaign', path: req.params.campaignId+'/'});
+
+    return next();
+  })
+  .get(function(req, res, next){
+    res.render('campaign');
+  })
+  .post(function(req, res, next){
+    // add or update campaign, default status - false
+    // if set campaign status to false, update campaign banners - set active false
+    res.redirect('/');
+  });
+
+router.route('/(:campaignId)/((:bannerId)|new)') // View and edit banner info
+  .all(function(req, res, next){
+    res.locals.title = 'Banner info';
+    res.locals.baseUrl = '/'+req.params.campaignId+'/';
+    for(let i in campaignList){
+      if(campaignList[i]._id == req.params.campaignId){
+        res.locals.campaign = campaignList[i];
+        let banners = campaignList[i].banners;
+        for(let j in banners){
+          if(banners[j]._id ==  req.params.bannerId){
+            res.locals.banner = banners[j];
+          }
+        }
+        if(!res.locals.banner) res.locals.banner = {};
+      }
+    }
+    res.locals.breadcrumbs.push({name: res.locals.campaign.name || 'New campaign', path: req.params.campaignId+'/'});
+    res.locals.breadcrumbs.push({name: res.locals.banner.name || 'New banner', path: req.params.bannerId+'/'});
+    return next();
+  })
+  .get(function(req, res, next){
+    res.render('banner')
+  })
+  .post(function(req, res, next){
+    // add or update banner
+    // if all campaign banners statuses is false, set campaign status to false
+    res.redirect('/')
+  });
+
+module.exports = router;
